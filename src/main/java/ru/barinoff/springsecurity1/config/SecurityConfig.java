b217e81b6eb87e4bf222cb65dd81abf4480aa07a
CunningLittleMouse <barinov.konstantin@bk.ru>
2022-03-08 12:49:47 +0300

    Вместо ролей ADMIN и USER добавлены соответсвующие Permission

--- src/main/java/ru/barinoff/springsecurity1/config/SecurityConfig.java
+++ src/main/java/ru/barinoff/springsecurity1/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ru.barinoff.springsecurity1.model.Permission;
 import ru.barinoff.springsecurity1.model.Role;
 
 @Configuration
@@ -22,9 +23,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests().antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers(HttpMethod.POST,"/api/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE,"/api/**").hasRole(Role.ADMIN.name())
+//                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .antMatchers(HttpMethod.GET,"/api/**").hasAnyAuthority(Permission.DEVELOPERS_READ.getPermission())
+//                .antMatchers(HttpMethod.POST,"/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.POST,"/api/**").hasAnyAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+//                .antMatchers(HttpMethod.DELETE,"/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE,"/api/**").hasAnyAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -39,12 +43,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 User.builder().
                         username("admin").
                         password(passwordEncoder().encode("admin")).
-                        roles(Role.ADMIN.name()).
+//                        roles(Role.ADMIN.name()).
+                        authorities(Role.ADMIN.getAuthorities()).
                         build(),
                 User.builder().
                         username("user").
                         password(passwordEncoder().encode("user")).
-                        roles(Role.USER.name()).
+//                        roles(Role.USER.name()).
+                        authorities(Role.USER.getAuthorities()).
                         build()
         );
     }
